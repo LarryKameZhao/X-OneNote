d7191adfd7a7f1ac6e84651d64f6c331b13ee592
LarryKameZhao <larryzhao1994@outlook.com>
2018-10-24 16:00:20 +0800

    'avatar'

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,22 +1,46 @@
 <template>
-    <span :title="user.username">{{slug}}</span>
+    <span :title="username">{{slug}}</span>
 </template>
 
 <script>
+    import Auth from '@/apis/auth'
   export default {
     data() {
       return {
-        user: {
-          username: 'SSS'
-        },
-        slug: 'S'
+        username: 'SSS',
       }
+    },
+    computed: {
+      slug () {
+        return this.username.charAt(0)
+      }
+    },
+    created () {
+      Auth.getInfo()
+        .then(res => {
+          console.log(res)
+          if (res.isLogin) {
+            this.username = res.data.username
+          }
+        })
     }
   }
 </script>
 
 <style scoped>
   span{
-      color: red;
+      display: inline-block;
+      width: 30px;
+      height: 30px;
+      text-align: center;
+      line-height: 32px;
+      border-radius: 50%;
+      background: #f2b81c;
+      color: #fff;
+      text-shadow: 1px 0 1px #795c19;
+      font-weight: bold;
+      text-transform: uppercase;
+      font-size: 18px;
+      margin-top: 15px;
   }
 </style>
